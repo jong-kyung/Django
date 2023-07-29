d6c458c9b608d87382fc03cc780b48651d21ba02
이종경 <gyoung3063413@naver.com>
2023-07-29 17:36:36 +0900

    [추가] Django post_detail 페이지 생성 및 매핑

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from app.models import Post
 
@@ -8,3 +8,10 @@ def index(request:HttpRequest) -> HttpResponse:
     return render(request, 'app/index.html', {
         'post_list': qs,
     } ) # 세번째 인자에서 참조를 시켜줌
+
+def post_detail(request: HttpRequest, pk :int) -> HttpResponse:
+    # post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post,pk=pk) # 해당 페이지가 없으면 404로 뜨게해줌
+    return render(request, 'app/post_detail.html',{
+        'post' : post,
+    })
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,6 @@ from app import views as app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', app_views.index)
+    path('app/', app_views.index),
+    path('app/<int:pk>/', app_views.post_detail), # url 매핑 설정
 ]
