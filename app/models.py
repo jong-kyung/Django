4c88bed23cda2480ec688a1c9d2f3c303aa14e89
이종경 <gyoung3063413@naver.com>
2023-08-16 20:36:54 +0900

    [추가] FK응용 데이터베이스 마이그레이션

--- app/models.py
+++ app/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length = 100) # 길이제한이 있는 문자열
     content = models.TextField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) # CASCADE 부모가 삭제되면 부모에 포함된 자식들도 삭제한다
+    mesaage = models.TextField()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
 class Article(models.Model):
     title = models.CharField(max_length = 100)
